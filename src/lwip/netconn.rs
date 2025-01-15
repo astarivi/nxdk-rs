30169a24bf3134633aa36f257c20a4e61e09815c
astarivi <astarivi@aol.com>
2025-01-15 18:04:48 -0500

    Doc Netconn impl

--- src/lwip/netconn.rs
+++ src/lwip/netconn.rs
@@ -10,27 +10,27 @@ use core::fmt;
 use core::net::Ipv4Addr;
 use core::ptr::null_mut;
 
+/// Error type for Netconn APIs. Names equivalent to enum names. Can be cast to the
+/// error number for inter-op.
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum NetconnErr {
-    Ok,
-    Mem,
-    Buf,
-    Timeout,
-    Rte,
-    InProgress,
-    Val,
-    WouldBlock,
-    Use,
-    Already,
-    IsConn,
-    Conn,
-    If,
-    Abrt,
-    Rst,
-    Clsd,
-    Arg,
-    Other,
-    ReadZero,
+    Ok = err_enum_t_ERR_OK as isize,
+    Mem = err_enum_t_ERR_MEM as isize,
+    Buf = err_enum_t_ERR_BUF as isize,
+    Timeout = err_enum_t_ERR_TIMEOUT as isize,
+    Rte = err_enum_t_ERR_RTE as isize,
+    InProgress = err_enum_t_ERR_INPROGRESS as isize,
+    Val = err_enum_t_ERR_VAL as isize,
+    WouldBlock = err_enum_t_ERR_WOULDBLOCK as isize,
+    Use = err_enum_t_ERR_USE as isize,
+    Already = err_enum_t_ERR_ALREADY as isize,
+    IsConn = err_enum_t_ERR_ISCONN as isize,
+    Conn = err_enum_t_ERR_CONN as isize,
+    If = err_enum_t_ERR_IF as isize,
+    Abrt = err_enum_t_ERR_ABRT as isize,
+    Rst = err_enum_t_ERR_RST as isize,
+    Clsd = err_enum_t_ERR_CLSD as isize,
+    Arg = err_enum_t_ERR_ARG as isize
 }
 
 impl NetconnErr {
@@ -53,8 +53,6 @@ impl NetconnErr {
             -14 => NetconnErr::Rst,
             -15 => NetconnErr::Clsd,
             -16 => NetconnErr::Arg,
-            -17 => NetconnErr::Other,
-            -18 => NetconnErr::ReadZero,
             _ => {
                 // Handle any other unexpected values
                 panic!("Unknown error code: {}", code);
@@ -79,17 +77,11 @@ impl Error for NetconnErr {}
 
 impl Display for NetconnErr {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            NetconnErr::Mem => write!(f, "Out of memory"),
-            NetconnErr::Buf => write!(f, "Buffer error"),
-            NetconnErr::Timeout => write!(f, "Timeout occurred"),
-            _ => {
-                write!(f, "{:?}", self)
-            }
-        }
+        write!(f, "{:?}", self)
     }
 }
 
+/// lwip Netconn type.
 #[derive(Default, Debug, PartialEq, Eq, Clone)]
 pub enum NetconnType {
     #[default]
@@ -106,6 +98,19 @@ pub enum NetconnType {
     Invalid = netconn_type_NETCONN_INVALID as isize
 }
 
+/// lwip Netconn Rust wrapper.
+///
+/// This implementation:
+///
+/// - Isn't thread-safe, but can be sent between threads
+/// - Has no IPv6 support
+/// - Has support for both TCP and UDP
+/// - Considers ERR_OK to be a successful call. Any other code is raised as Err
+/// - Works with blocking and non-blocking modes (`set_nonblocking(bool)`)
+/// - Timeouts aren't implemented; prefer non-blocking over timeout
+///
+/// Remember to call `nxdk::net::nx_net_init()` before using a Netconn.
+/// Failing to do so will result in unexpected behavior.
 #[derive(Eq, PartialEq)]
 pub struct Netconn {
     conn: Option<*mut netconn>,
@@ -113,6 +118,9 @@ pub struct Netconn {
 }
 
 impl Netconn {
+    /// Create new netconn connection
+    ///
+    /// API: `Common`
     pub fn new(netconn_type: NetconnType) -> Result<Self, NetconnErr> {
         let conn = unsafe {
             netconn_new_with_proto_and_callback(
@@ -130,13 +138,98 @@ impl Netconn {
             conn: Some(conn),
             conn_type: netconn_type
         })
-
     }
 
     fn get_inner(&self) -> Result<*mut netconn, NetconnErr> {
         self.conn.ok_or(NetconnErr::Clsd)
     }
 
+    /// Get the local or remote IP address and port of a netconn.
+    /// For RAW netconns, this returns the protocol instead of a port!
+    ///
+    /// Implementation note: This method returns a raw ip_addr_t, intended
+    /// for inter-op with other lwip functions. Prefer `peer()` or `addr()` over this
+    /// for other use cases.
+    /// 
+    /// API: `Common`
+    pub fn getaddr_native(&self, local: bool) -> Result<(ip_addr_t, u16), NetconnErr> {
+        let mut ipaddr: ip_addr_t = unsafe { core::mem::zeroed() };
+        let mut port: u16 = 0;
+
+        let result = unsafe {
+            netconn_getaddr(
+                self.get_inner()?,
+                &mut ipaddr,
+                &mut port,
+                if local {1} else {0}
+            )
+        };
+
+        if result != err_enum_t_ERR_OK as i8 {
+            return Err(NetconnErr::from(result));
+        }
+
+        Ok((ipaddr, port))
+    }
+
+    /// Get the remote peer IP address and port of a netconn.
+    /// For RAW netconns, this returns the protocol instead of a port!
+    ///
+    /// API: `Common`
+    pub fn peer(&self) -> Result<(Ipv4Addr, u16), NetconnErr> {
+        let ipaddr = self.getaddr_native(false)?;
+        Ok((native_ipv4_to_local(&ipaddr.0), ipaddr.1))
+    }
+
+    /// Get the local IP address and port of a netconn.
+    /// For RAW netconns, this returns the protocol instead of a port!
+    ///
+    /// API: `Common`
+    pub fn addr(&self) -> Result<(Ipv4Addr, u16), NetconnErr> {
+        let ipaddr = self.getaddr_native(true)?;
+        Ok((native_ipv4_to_local(&ipaddr.0), ipaddr.1))
+    }
+
+    /// Close a netconn 'connection' and free all its resources but
+    /// not the netconn itself. UDP and RAW connection are completely
+    /// closed, TCP pcbs might still be in a waitstate after this returns.
+    ///
+    /// API: `Common`
+    pub fn prepare_delete(&self) -> Result<(), NetconnErr> {
+        let result = unsafe {
+            netconn_prepare_delete(
+                self.get_inner()?,
+            )
+        };
+
+        if result != err_enum_t_ERR_OK as i8 {
+            return Err(NetconnErr::from(result));
+        }
+
+        Ok(())
+    }
+
+    /// Close a netconn 'connection' and free its resources.
+    /// UDP and RAW connection are completely closed,
+    /// TCP pcbs might still be in a waitstate after this returns.
+    ///
+    /// Implementation note: Further calls to a deleted Netconn will
+    /// always yield error `Clsd`
+    /// 
+    /// API: `Common`
+    pub fn delete(&mut self) {
+        if let Some(conn) = self.conn.take() {
+            unsafe {
+                netconn_delete(conn);
+            }
+        };
+    }
+
+    /// Bind a netconn to a specific local IP address and port.
+    /// Binding one netconn twice might not always be checked correctly!
+    ///
+    /// Implementation note: This binds to all addresses.
+    /// API: `Common`
     pub fn bind(&mut self, port: u16) -> Result<(), NetconnErr> {
         let result = unsafe {
             netconn_bind(
@@ -153,6 +246,11 @@ impl Netconn {
         Ok(())
     }
 
+    /// Bind a netconn to a specific local IP address and port.
+    /// Binding one netconn twice might not always be checked correctly!
+    ///
+    /// Implementation note: This binds only to the given address.
+    /// API: `Common`
     pub fn bind_to(&mut self, ip: &Ipv4Addr, port: u16) -> Result<(), NetconnErr> {
         let result = unsafe {
             netconn_bind(
@@ -169,6 +267,66 @@ impl Netconn {
         Ok(())
     }
 
+    /// Connect a netconn to a specific remote IP address and port.
+    ///
+    /// Implementation note: This binds only to the given address.
+    /// API: `Common`
+    pub fn connect(&mut self, addr: &Ipv4Addr, port: u16) -> Result<(), NetconnErr> {
+        let err = unsafe {
+            netconn_connect(
+                self.get_inner()?,
+                &local_ipv4_to_native(addr) as *const ip_addr,
+                port
+            )
+        };
+
+        if err != err_enum_t_ERR_OK as i8 {
+            return Err(NetconnErr::from(err));
+        }
+
+        Ok(())
+    }
+
+    /// Get and reset pending error on a netconn
+    /// 
+    /// API: `Common`
+    pub fn err(&self) -> NetconnErr {
+        let conn: *mut netconn = match self.get_inner() {
+            Ok(x) => {x}
+            Err(e) => {
+                return e;
+            }
+        };
+
+        NetconnErr::from(unsafe {
+            netconn_err(
+                conn
+            )
+        })
+    }
+
+    /// Set the non-blocking flag for this netconn to the given value. Default is false.
+    ///
+    /// API: `Common`
+    pub fn set_nonblocking(&self, value: bool) -> Result<(), NetconnErr> {
+        if value {
+            unsafe {
+                let inner_con = self.get_inner()?;
+                (*inner_con).flags |= NETCONN_FLAG_NON_BLOCKING as u8;
+            }
+        } else {
+            unsafe {
+                let inner_con = self.get_inner()?;
+                (*inner_con).flags &= !(NETCONN_FLAG_NON_BLOCKING as u8);
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Set a TCP netconn into listen mode
+    /// 
+    /// API: `TCP`
     pub fn listen(&mut self) -> Result<(), NetconnErr> {
         let _new_pcb = unsafe {
             netconn_listen_with_backlog(
@@ -180,6 +338,9 @@ impl Netconn {
         Ok(())
     }
 
+    /// Accept a new connection on a TCP listening netconn
+    ///
+    /// API: `TCP`
     pub fn accept(&mut self) -> Result<Option<Self>, NetconnErr> {
         let mut new_conn: *mut netconn = null_mut();
 
@@ -204,78 +365,6 @@ impl Netconn {
         }))
     }
 
-    pub fn get_err(&self) -> NetconnErr {
-        let conn: *mut netconn = match self.get_inner() {
-            Ok(x) => {x}
-            Err(e) => {
-                return e;
-            }
-        };
-
-        NetconnErr::from(unsafe {
-            netconn_err(
-                conn
-            )
-        })
-    }
-
-    pub fn set_nonblocking(&self, value: bool) -> Result<(), NetconnErr> {
-        if value {
-            unsafe {
-                let inner_con = self.get_inner()?;
-                (*inner_con).flags |= NETCONN_FLAG_NON_BLOCKING as u8;
-            }
-        } else {
-            unsafe {
-                let inner_con = self.get_inner()?;
-                (*inner_con).flags &= !(NETCONN_FLAG_NON_BLOCKING as u8);
-            }
-        }
-
-        Ok(())
-    }
-
-    pub fn get_address_native(&self) -> Result<(ip_addr_t, u16), NetconnErr> {
-        let mut ipaddr: ip_addr_t = unsafe { core::mem::zeroed() };
-        let mut port: u16 = 0;
-
-        let result = unsafe {
-            netconn_getaddr(
-                self.get_inner()?,
-                &mut ipaddr,
-                &mut port,
-                0
-            )
-        };
-
-        if result != err_enum_t_ERR_OK as i8 {
-            return Err(NetconnErr::from(result));
-        }
-
-        Ok((ipaddr, port))
-    }
-
-    pub fn get_address(&self) -> Result<Ipv4Addr, NetconnErr> {
-        let ipaddr = self.get_address_native()?;
-        Ok(native_ipv4_to_local(&ipaddr.0))
-    }
-
-    pub fn connect(&mut self, addr: &Ipv4Addr, port: u16) -> Result<(), NetconnErr> {
-        let err = unsafe {
-            netconn_connect(
-                self.get_inner()?,
-                &local_ipv4_to_native(addr) as *const ip_addr,
-                port
-            )
-        };
-
-        if err != err_enum_t_ERR_OK as i8 {
-            return Err(NetconnErr::from(err));
-        }
-
-        Ok(())
-    }
-
     pub fn read_no_copy(&mut self) -> Result<TcpPbuf, NetconnErr>{
         let mut pbuf_ptr: *mut pbuf = null_mut();
 
@@ -291,7 +380,7 @@ impl Netconn {
         }
 
         if pbuf_ptr.is_null() {
-            return Err(NetconnErr::ReadZero);
+            return Err(NetconnErr::Mem);
         }
 
         Ok(TcpPbuf::new(pbuf_ptr))
@@ -347,10 +436,14 @@ impl Write for Netconn {
 impl Read for Netconn {
     type ReadError = NetconnErr;
 
-    /// This is an unoptimized way of reading, and uses copy.
+    /// This is an unoptimized way of reading that copies data to the given buffer.
     /// If there's more data available than it fits in given buf, it's discarded.
     /// 
     /// ## Use read_no_copy() instead of this method for large transfers
+    /// 
+    /// Implementation note: This is equivalent to `netbuff_recv`
+    /// 
+    /// API: `Common`
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::ReadError> {
         let mut pbuf = self.read_no_copy()?;
         let mut total_written: usize = 0;
@@ -376,7 +469,7 @@ unsafe impl Send for Netconn {}
 
 pub fn get_host_by_name_native(hostname: &str) -> Result<ip_addr_t, NetconnErr>{
     let mut ipaddr: ip_addr_t = unsafe { core::mem::zeroed() };
-    let hostname_c = CString::new(hostname).map_err(|e| NetconnErr::Other)?;
+    let hostname_c = CString::new(hostname).map_err(|e| NetconnErr::Mem)?;
 
     let err = unsafe {
         netconn_gethostbyname_addrtype(
